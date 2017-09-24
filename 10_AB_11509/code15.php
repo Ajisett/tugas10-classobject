6ab1717dd9b285bd6c71132001a0f134170babdb
Ajisett <aryantoasa@gmail.com>
2017-09-24 22:42:55 +0700

    mengumpulkan tugas 10

---
+++ 10_AB_11509/code15.php
@@ -0,0 +1,36 @@
+<?php
+// buat abstract class
+abstract class smartphone{
+	abstract public function lihat_spec();
+	abstract public function lihat_processor();
+	abstract public function lihat_harddisk();
+	abstract public function lihat_pemilik();
+}
+class tablet extends smartphone{
+	public static function hidupkan_komputer(){
+		echo "Hidupkan Komputer";
+	}
+	public function lihat_spec(){
+		return "Lihat spec tablet...";
+	}
+	public function lihat_processor(){
+		return "Lihat Processor tablet...";
+	}
+	public function lihat_harddisk(){
+		return "Lihat harddisk tablet...";
+	}
+	public function lihat_pemilik(){
+		return "Lihat pemilik tablet...";
+	}
+}
+$tablet_obj = new tablet();
+echo $tablet_obj->hidupkan_komputer();
+echo "<br/ >";
+echo $tablet_obj->lihat_spec();
+echo "<br/ >";
+echo $tablet_obj->lihat_processor();
+echo "<br/ >";
+echo $tablet_obj->lihat_harddisk();
+echo "<br/ >";
+echo $tablet_obj->lihat_pemilik();
+?>
---
+++ 10_AB_11509/code4.php
@@ -0,0 +1,30 @@
+<?php
+class smartphone {
+	public function __construct() {
+		echo "Constructor dari class smartphone <br />";
+		}
+	public function __destruct() {
+		echo "Destructor dari class smartphone <br />";
+		}
+	}
+	class smartphone_dualsim extends smartphone {
+	public function __construct() {
+		echo "Constructor dari class smartphone_dualsim <br />";
+		}
+	public function __destruct() {
+		echo "Destructor dari class smartphone_dualsim <br />";
+		}
+	}
+	class smartphone_singlesim extends smartphone_dualsim {
+		public function __construct() {
+			echo "Constructor dari class smartphone_singlesim <br />";
+		}
+		public function __destruct() {
+			echo "Destructor dari class smartphone_singlesim <br />";
+		}
+	}
+	$gadget_baru_construct = new smartphone();
+	$gadget_baru_construct = new smartphone_dualsim();
+	echo "Belajar OOP PHP <br />";
+	$gadget_baru_construct = new smartphone_singlesim();
+?>
